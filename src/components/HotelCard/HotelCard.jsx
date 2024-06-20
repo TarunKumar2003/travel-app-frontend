bf08409646feb1cc036adf6f8bde03a341774dea
Tarun Kumar <tarunnimbal123@gmail.com>
2024-06-20 11:24:56 +0530

    Install Axios and configure tailwind and fetch Hotel Data show to the UI

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -1,36 +1,31 @@
 import './HotelCard.css'
-export function HotelCard() {
+export function HotelCard({ hotel }) {
+  // Destructure All of things
+  const { image, address, rating, name, price, state } = hotel
   return (
     <div className="relative hotelcard-container shadow cursor-pointer">
       <div>
-        <img
-          className="img"
-          src="https://images.pexels.com/photos/25255070/pexels-photo-25255070/free-photo-of-two-women-walking-down-a-narrow-alleyway-in-a-small-town.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-          alt="hotel-card"
-        />
+        <img className="img" src={image} alt="hotel-card" />
         <div className="hotelcard-details">
           <div className="d-flex align-center">
-            <span className="location"> Kurukshetra</span>
+            <span className="location">
+              {' '}
+              {address}, {state}
+            </span>
             <span className="rating d-flex align-center">
-              <span class="material-icons-outlined">star</span>
+              <span className="material-icons-outlined">{rating}</span>
               <span></span>
             </span>
           </div>
-          <p className="hotel-name">Zion</p>
+          <p className="hotel-name">{name}</p>
           <p className="price-details">
-            <span className="price">Rs. 800</span>
+            <span className="price">Rs. {price}</span>
             <span>night</span>
           </p>
         </div>
       </div>
       <button className="button btn-wishlist absolute d-flex align-center">
-        <span
-        // className={`material-icons favorite cursor ${
-        //   isHotelInWishlist ? 'fav-selected' : ''
-        // }`}
-        >
-          favorite
-        </span>
+        <span className="material-icons favorite cursor">favorite</span>
       </button>
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
 import { HotelCard, Navbar } from '../../components'
 import './Home.css'
+import axios from 'axios'
+
 export function Home() {
+  const [hotels, setHotels] = useState([])
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const { data } = await axios.get(
+          'http://localhost:3000/api/v1/hotels/all-hotels'
+        )
+        ///console.log(data.data)
+        setHotels(data.data)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [])
   return (
     <>
       <Navbar />
-      <main className="main">
-        <HotelCard />
+      <main className="main d-flex flex-wrap ">
+        {hotels &&
+          hotels.map((hotel) => <HotelCard key={hotel._id} hotel={hotel} />)}
       </main>
     </>
   )
